0ac4fbc817a2b571600e260b6174b8a9eda006c2
Antonio Bandeira <104794351+antonioviniciusvb@users.noreply.github.com>
2023-11-27 15:28:52 -0300

    Repository implementation

---
+++ src/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package repository;
+
+import java.util.Optional;
+
+import model.Product;
+
+public interface ProductRepository {
+	public void register(Product product);
+	public void listAll();
+	public void findById(int id);
+	public void findByName(String name);
+	public void findByplatform(int platform);
+	public void delete(int id);
+	public void update(Optional<Product> product);
+}
